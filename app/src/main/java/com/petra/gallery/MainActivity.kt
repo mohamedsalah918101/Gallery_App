021f6227ff5c9a19dc407342ac031912932ce6f3
Mohamed <mohamedsalah3072001@gmail.com>
2024-02-19 18:40:27 +0200

    update code

--- app/src/main/java/com/petra/gallery/MainActivity.kt
+++ app/src/main/java/com/petra/gallery/MainActivity.kt
@@ -2,8 +2,8 @@ package com.petra.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.FrameLayout
 import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
@@ -11,69 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image1 = findViewById(R.id.image1) as ImageView
-        val image2 = findViewById(R.id.image2) as ImageView
-        val image3 = findViewById(R.id.image3) as ImageView
-        val image4 = findViewById(R.id.image4) as ImageView
-        val image5 = findViewById(R.id.image5) as ImageView
-        val image6 = findViewById(R.id.image6) as ImageView
-        val image7 = findViewById(R.id.image7) as ImageView
-        val image8 = findViewById(R.id.image8) as ImageView
-        val image9 = findViewById(R.id.image9) as ImageView
-        val image10 = findViewById(R.id.image10) as ImageView
-
         val frameLayout = findViewById(R.id.frameLayout) as FrameLayout
 
-        image1.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image2.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image3.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image4.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image5.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image6.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image7.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image8.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image9.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
-        image10.setOnClickListener(){
-            val fragment = viewImage()
-            val fragmentTransaction:FragmentTransaction = supportFragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
-        }
+        val imageIds = listOf(
+            R.id.image1, R.id.image2, R.id.image3, R.id.image4, R.id.image5,
+            R.id.image6, R.id.image7, R.id.image8, R.id.image9, R.id.image10
+        )
 
+        for (imageId in imageIds) {
+            findViewById<ImageView>(imageId).setOnClickListener {
+                val fragment = viewImage()
+                val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
+            }
+        }
     }
 }
--- app/src/main/java/com/petra/gallery/viewImage.kt
+++ app/src/main/java/com/petra/gallery/viewImage.kt
@@ -9,64 +9,30 @@ import android.widget.ImageView
 
 class viewImage : Fragment() {
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_view_image, container, false)
 
-        val Image1 = requireActivity().findViewById(R.id.image1) as ImageView
-        val Image2 = requireActivity().findViewById(R.id.image2) as ImageView
-        val Image3 = requireActivity().findViewById(R.id.image3) as ImageView
-        val Image4 = requireActivity().findViewById(R.id.image4) as ImageView
-        val Image5 = requireActivity().findViewById(R.id.image5) as ImageView
-        val Image6 = requireActivity().findViewById(R.id.image6) as ImageView
-        val Image7 = requireActivity().findViewById(R.id.image7) as ImageView
-        val Image8 = requireActivity().findViewById(R.id.image8) as ImageView
-        val Image9 = requireActivity().findViewById(R.id.image9) as ImageView
-        val Image10 = requireActivity().findViewById(R.id.image10) as ImageView
+        val imageIds = listOf(
+            R.drawable.buffon, R.drawable.varane, R.drawable.ramos,
+            R.drawable.marcelo, R.drawable.casemiro, R.drawable.modric,
+            R.drawable.kroos, R.drawable.messi, R.drawable.leva2,
+            R.drawable.ronaldo
+        )
 
-        var viewImage = view.findViewById(R.id.viewImage) as ImageView
+        val viewImage = view.findViewById(R.id.viewImage) as ImageView
 
-        Image1.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.buffon)
-        }
-        Image2.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.varane)
-        }
-        Image3.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.ramos)
-        }
-        Image4.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.marcelo)
-        }
-        Image5.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.casemiro)
-        }
-        Image6.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.modric)
-        }
-        Image7.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.kroos)
-        }
-        Image8.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.messi)
-        }
-        Image9.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.leva2)
-        }
-        Image10.setOnClickListener(){
-            viewImage.setImageResource(R.drawable.ronaldo)
+        val imageClickListeners = listOf(
+            R.id.image1, R.id.image2, R.id.image3, R.id.image4, R.id.image5,
+            R.id.image6, R.id.image7, R.id.image8, R.id.image9, R.id.image10
+        ).mapIndexed { index, imageId ->
+            requireActivity().findViewById<ImageView>(imageId).setOnClickListener {
+                viewImage.setImageResource(imageIds[index])
+            }
         }
 
         return view
     }
-
-
-
-
-
 }
